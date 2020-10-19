3434a916eb6d2a69da177bcfdedb9b3e99855416
Brais Cidras <braisomary@gmail.com>
2020-10-19 21:24:59 +0200

    Implementing DAO with CrudRepository interface

--- src/main/java/com/merlobranco/springboot/app/models/dao/ClienteDao.java
+++ src/main/java/com/merlobranco/springboot/app/models/dao/ClienteDao.java
@@ -1,16 +1,8 @@
 package com.merlobranco.springboot.app.models.dao;
 
-import java.util.List;
+import org.springframework.data.repository.CrudRepository;
 
 import com.merlobranco.springboot.app.models.entity.Cliente;
 
-public interface ClienteDao {
-	
-	public List<Cliente> findAll();
-	
-	public Cliente findOne(Long id);
-	
-	public void save(Cliente cliente);
-	
-	public void delete(Long id);
+public interface ClienteDao extends CrudRepository<Cliente, Long>{
 }
--- src/main/java/com/merlobranco/springboot/app/models/service/ClienteServiceImpl.java
+++ src/main/java/com/merlobranco/springboot/app/models/service/ClienteServiceImpl.java
@@ -18,13 +18,13 @@ public class ClienteServiceImpl implements ClienteService {
 	@Override
 	@Transactional(readOnly=true)
 	public List<Cliente> findAll() {
-		return clienteDao.findAll();
+		return (List<Cliente>)clienteDao.findAll();
 	}
 
 	@Override
 	@Transactional(readOnly=true)
 	public Cliente findOne(Long id) {
-		return clienteDao.findOne(id);
+		return clienteDao.findById(id).orElse(null);
 	}
 
 	@Override
@@ -36,7 +36,7 @@ public class ClienteServiceImpl implements ClienteService {
 	@Override
 	@Transactional
 	public void delete(Long id) {
-		clienteDao.delete(id);
+		clienteDao.deleteById(id);
 	}
 
 }
