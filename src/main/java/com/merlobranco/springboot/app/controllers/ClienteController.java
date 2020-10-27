9e90d217bb74aa21431bc9e5fac84991cfb3d95c
Brais Cidras <braisomary@gmail.com>
2020-10-27 20:42:59 +0200

    Loading the image programmatically in the HTTP Response - using ResponseEntity

--- src/main/java/com/merlobranco/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/merlobranco/springboot/app/controllers/ClienteController.java
@@ -1,6 +1,7 @@
 package com.merlobranco.springboot.app.controllers;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -11,9 +12,13 @@ import javax.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -40,6 +45,23 @@ public class ClienteController {
 	private ClienteService clienteService;
 	
 	private final Logger log = LoggerFactory.getLogger(getClass());
+	
+	@GetMapping("/uploads/{filename:.+}")
+	public ResponseEntity<Resource> verFoto(@PathVariable String filename) {
+		Path pathFoto = Paths.get("uploads").resolve(filename).toAbsolutePath();
+		log.info("pathFoto: " + pathFoto);
+		Resource recurso =  null;
+		try {
+			recurso = new UrlResource(pathFoto.toUri());
+			if (!recurso.exists() || !recurso.isReadable()) {
+				throw new RuntimeException("Error: no se puede cargar la imagen " + pathFoto.toString());
+			}
+		} catch (MalformedURLException e) {
+			log.error(e.getMessage());
+		}
+		
+		return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + recurso.getFilename() + "\"").body(recurso);
+	}
 	
 	@GetMapping("/ver/{id}")
 	public String ver(@PathVariable(value="id") Long id, Model model, RedirectAttributes flash) {
