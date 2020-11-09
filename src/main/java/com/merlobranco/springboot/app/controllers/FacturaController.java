f32ddede1e461314e8ff75daa2b09d28172af679
Brais Cidras <braisomary@gmail.com>
2020-11-09 12:39:41 +0330

    Adding FacturaController with crear method handler

---
+++ src/main/java/com/merlobranco/springboot/app/controllers/FacturaController.java
@@ -0,0 +1,41 @@
+package com.merlobranco.springboot.app.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.merlobranco.springboot.app.models.entity.Cliente;
+import com.merlobranco.springboot.app.models.entity.Factura;
+import com.merlobranco.springboot.app.models.service.ClienteService;
+
+@Controller
+@RequestMapping("/factura")
+@SessionAttributes("factura")
+public class FacturaController {
+	
+	@Autowired
+	private ClienteService clienteService;
+	
+	@GetMapping("/form/{clienteId}")
+	public String crear(@PathVariable(value="clienteId") Long clienteId, Model model, RedirectAttributes flash) {
+		
+		Cliente cliente = clienteService.findOne(clienteId);
+		if (cliente == null) {
+			flash.addFlashAttribute("error", "El cliente no existe en la base de datos");
+			return "redirect:/listar";
+		}
+		
+		Factura factura = new Factura();
+		factura.setCliente(cliente);
+		model.addAttribute("factura", factura);
+		model.addAttribute("titulo", "Crear Factura");
+		
+		return "/factura/form";
+	}
+
+}
