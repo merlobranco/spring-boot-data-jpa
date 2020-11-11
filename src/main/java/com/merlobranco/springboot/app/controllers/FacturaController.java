05d952f7b97dc0ca82bdf61e319cf39d0054e785
Brais Cidras <braisomary@gmail.com>
2020-11-11 14:13:20 +0330

    Adding save method handler to FacturaController

--- src/main/java/com/merlobranco/springboot/app/controllers/FacturaController.java
+++ src/main/java/com/merlobranco/springboot/app/controllers/FacturaController.java
@@ -2,18 +2,24 @@ package com.merlobranco.springboot.app.controllers;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.merlobranco.springboot.app.models.entity.Cliente;
 import com.merlobranco.springboot.app.models.entity.Factura;
+import com.merlobranco.springboot.app.models.entity.ItemFactura;
 import com.merlobranco.springboot.app.models.entity.Producto;
 import com.merlobranco.springboot.app.models.service.ClienteService;
 
@@ -21,6 +27,8 @@ import com.merlobranco.springboot.app.models.service.ClienteService;
 @RequestMapping("/factura")
 @SessionAttributes("factura")
 public class FacturaController {
+	
+	private final Logger log = LoggerFactory.getLogger(getClass());
 	
 	@Autowired
 	private ClienteService clienteService;
@@ -45,5 +53,24 @@ public class FacturaController {
 	@GetMapping(value = "/cargar-productos/{term}", produces = {"application/json"})
 	public @ResponseBody List<Producto> cargarProductos(@PathVariable(value="term") String term) {
 		return clienteService.findByNombre(term);
+	}
+	
+	@PostMapping("/form")
+	public String guardar(Factura factura, @RequestParam(name="item_id[]", required=false) Long[] itemId, @RequestParam(name="cantidad[]", required=false) Integer[] cantidad, RedirectAttributes flash, SessionStatus status) {
+		for (int i = 0; i < itemId.length; i++) {
+			Producto producto = clienteService.findProductoById(itemId[i]);
+			ItemFactura linea = new ItemFactura();
+			linea.setCantidad(cantidad[i]);
+			linea.setProducto(producto);
+			factura.addItemFactura(linea);
+			
+			log.info("ID: " + itemId[i].toString() + ", cantidad: " + cantidad[i].toString());
+		}
+		clienteService.saveFactura(factura);
+		status.setComplete();
+		
+		flash.addFlashAttribute("success", "Factura creada con éxito!");
+		
+		return "redirect:/ver/" + factura.getCliente().getId();
 	}
 }
