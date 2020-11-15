909b174ab265249799cc77430dc2b128954c97b2
Brais Cidras <braisomary@gmail.com>
2020-11-15 11:06:57 +0330

    Adding SpringSecurityConfig class for registering users

---
+++ src/main/java/com/merlobranco/springboot/app/SpringSecurityConfig.java
@@ -0,0 +1,30 @@
+package com.merlobranco.springboot.app;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder build) throws Exception {
+		PasswordEncoder encoder = passwordEncoder();
+		UserBuilder users = User.builder().passwordEncoder(encoder::encode);
+		
+		build.inMemoryAuthentication()
+		.withUser(users.username("admin").password("12345").roles("ADMIN", "USER"))
+		.withUser(users.username("brais").password("12345").roles("USER"));
+	}
+}
