90ff06c34d383578fb2f7031f16e0c732af76d74
Brais Cidras <braisomary@gmail.com>
2020-11-18 18:19:39 +0330

    Adding BCrypt password encoder

--- src/main/java/com/merlobranco/springboot/app/SpringSecurityConfig.java
+++ src/main/java/com/merlobranco/springboot/app/SpringSecurityConfig.java
@@ -1,7 +1,6 @@
 package com.merlobranco.springboot.app;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -21,14 +20,12 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private LoginSuccessHandler successHandler;
 	
-	@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
 	
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder build) throws Exception {
-		PasswordEncoder encoder = passwordEncoder();
+		PasswordEncoder encoder = passwordEncoder;
 		UserBuilder users = User.builder().passwordEncoder(encoder::encode);
 		
 		build.inMemoryAuthentication()
