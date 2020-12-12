c6b7cfce5d090e0440eaa7994f46af084f96b964
Brais Cidras <braisomary@gmail.com>
2020-12-12 19:03:37 +0200

    Adding language support to PDF view

--- src/main/java/com/merlobranco/springboot/app/view/pdf/FacturaPdfView.java
+++ src/main/java/com/merlobranco/springboot/app/view/pdf/FacturaPdfView.java
@@ -1,12 +1,17 @@
 package com.merlobranco.springboot.app.view.pdf;
 
 import java.awt.Color;
+import java.util.Locale;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.view.document.AbstractPdfView;
 
 import com.lowagie.text.Document;
@@ -19,10 +24,22 @@ import com.merlobranco.springboot.app.models.entity.ItemFactura;
 
 @Component("/factura/ver")
 public class FacturaPdfView extends AbstractPdfView {
+	
+	@Autowired
+    private MessageSource messageSource;
+	
+	@Autowired
+    private LocaleResolver localeResolver;
 
 	@Override
 	protected void buildPdfDocument(Map<String, Object> model, Document document, PdfWriter writer,
 			HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		Locale locale = localeResolver.resolveLocale(request);
+		
+		// Other alternative of retrieving the translated content
+		// Manage the locale inside so we could translate the texts directly
+		MessageSourceAccessor mensajes =  getMessageSourceAccessor();
 		
 		Factura factura = (Factura)model.get("factura");
 		
@@ -31,7 +48,7 @@ public class FacturaPdfView extends AbstractPdfView {
 		tCliente.setSpacingAfter(20);
 		
 		// Customer title
-		PdfPCell cell =  new PdfPCell(new Phrase("Datos del Cliente"));
+		PdfPCell cell = new PdfPCell(new Phrase(messageSource.getMessage("text.factura.ver.datos.cliente", null, locale)));
 		cell.setBackgroundColor(new Color(184, 218, 255));
 		cell.setPadding(8f);
 		
@@ -44,22 +61,22 @@ public class FacturaPdfView extends AbstractPdfView {
 		tFactura.setSpacingAfter(20);
 		
 		// Invoice title
-		cell =  new PdfPCell(new Phrase("Datos de la Factura"));
+		cell = new PdfPCell(new Phrase(messageSource.getMessage("text.factura.ver.datos.factura", null, locale)));
 		cell.setBackgroundColor(new Color(195, 230, 203));
 		cell.setPadding(8f);
 		
 		tFactura.addCell(cell);
-		tFactura.addCell("Folio: " + factura.getId());
-		tFactura.addCell("Descripción: " + factura.getDescripcion());
-		tFactura.addCell("Fecha: " + factura.getCreateAt());
+		tFactura.addCell(mensajes.getMessage("text.cliente.factura.folio") + ": " +  factura.getId());
+		tFactura.addCell(mensajes.getMessage("text.cliente.factura.descripcion") + ": " + factura.getDescripcion());
+		tFactura.addCell(mensajes.getMessage("text.cliente.factura.fecha") + ": " + factura.getCreateAt());
 		
 		// Table with invoice lines data
 		PdfPTable tDetalleFactura = new PdfPTable(4);
 		tDetalleFactura.setWidths(new float [] {3.5f, 1, 1, 1});
-		tDetalleFactura.addCell("Producto");
-		tDetalleFactura.addCell("Precio");
-		tDetalleFactura.addCell("Cantidad");
-		tDetalleFactura.addCell("Total");
+		tDetalleFactura.addCell(mensajes.getMessage("text.factura.form.item.nombre"));
+		tDetalleFactura.addCell(mensajes.getMessage("text.factura.form.item.precio"));
+		tDetalleFactura.addCell(mensajes.getMessage("text.factura.form.item.cantidad"));
+		tDetalleFactura.addCell(mensajes.getMessage("text.factura.form.item.total"));
 		
 		for (ItemFactura item : factura.getItems()) {
 			tDetalleFactura.addCell(item.getProducto().getNombre());
@@ -73,7 +90,7 @@ public class FacturaPdfView extends AbstractPdfView {
 			tDetalleFactura.addCell(item.calcularImporte().toString());
 		}
 		
-		cell = new PdfPCell(new Phrase("Total: "));
+		cell = new PdfPCell(new Phrase(mensajes.getMessage("text.factura.form.total") + ": "));
 		cell.setColspan(3);
 		cell.setHorizontalAlignment(PdfPCell.ALIGN_RIGHT);
 		tDetalleFactura.addCell(cell);
