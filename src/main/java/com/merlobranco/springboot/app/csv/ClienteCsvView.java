18178fe9db72cb5e1d80f940cac5186bd0176da6
Brais Cidras <braisomary@gmail.com>
2020-12-13 20:41:15 +0200

    Having more than one view with @Component and media types

--- src/main/java/com/merlobranco/springboot/app/csv/ClienteCsvView.java
+++ src/main/java/com/merlobranco/springboot/app/csv/ClienteCsvView.java
@@ -14,7 +14,7 @@ import org.supercsv.prefs.CsvPreference;
 
 import com.merlobranco.springboot.app.models.entity.Cliente;
 
-@Component("/listar")
+@Component("/listar.csv")
 public class ClienteCsvView extends AbstractView {
 	
 	public ClienteCsvView() {
