4b7f9104e1bbbeeb7f17726de99e473d6300a58c
Brais Cidras <braisomary@gmail.com>
2020-12-13 20:36:21 +0200

    ClienteCsvView implementation and adding clients.csv link

--- src/main/java/com/merlobranco/springboot/app/csv/ClienteCsvView.java
+++ src/main/java/com/merlobranco/springboot/app/csv/ClienteCsvView.java
@@ -5,8 +5,14 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.view.AbstractView;
+import org.supercsv.io.CsvBeanWriter;
+import org.supercsv.io.ICsvBeanWriter;
+import org.supercsv.prefs.CsvPreference;
+
+import com.merlobranco.springboot.app.models.entity.Cliente;
 
 @Component("/listar")
 public class ClienteCsvView extends AbstractView {
@@ -22,7 +28,7 @@ public class ClienteCsvView extends AbstractView {
 		return true;
 	}
 	
-	
+	@SuppressWarnings("unchecked")
 	@Override
 	protected void renderMergedOutputModel(Map<String, Object> model, HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
@@ -30,5 +36,18 @@ public class ClienteCsvView extends AbstractView {
 		// Providing a custom name to the file
 		response.setHeader("Content-Disposition", "attachment; filename=\"clientes.csv\"");
 		response.setContentType(getContentType());
+		
+		Page<Cliente> clientes = (Page<Cliente>)model.get("clientes");
+		
+		ICsvBeanWriter beanWriter = new CsvBeanWriter(response.getWriter(),  CsvPreference.STANDARD_PREFERENCE);
+		
+		String[] header = {"id", "nombre", "apellido", "email", "createAt"};
+		beanWriter.writeHeader(header);
+		
+		for (Cliente cliente : clientes) {
+			beanWriter.write(cliente, header);
+		}
+		
+		beanWriter.close();
 	}
 }
